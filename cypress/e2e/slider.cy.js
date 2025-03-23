e7e1e4361581a1579725ab04b7f0e0988f18e097
wiktorwozny <wikivip10@gmail.com>
2025-03-23 21:50:03 +0100

    added cypress test and hotjar integration

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,27 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  const slides = [
+    { title: 'Rome', description: 'Italy' },
+    { title: 'London', description: 'United Kingdom' },
+    { title: 'Paris', description: 'France' }
+  ];
+
+  it('Checks if each slide displays the correct title and description', function () {
+    cy.visit('http://localhost:3000');
+
+    slides.forEach((slide, index) => {
+      if (index > 0) {
+        cy.get('.swiper-button-next').click();
+        cy.wait(2000);
+      }
+      
+      cy.get('.swiper-slide-active').within(() => {
+        cy.contains('h1', slide.title).should('be.visible');
+        cy.contains('p', slide.description).should('be.visible');
+      });
+    });
+  });
+});
